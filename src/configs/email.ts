4aef95175dcab0447d83e91f6b2bb0beee54d9cd
GioMjds <giomjds@gmail.com>
2025-08-14 17:43:02 +0800

    init project

---
+++ src/configs/email.ts
@@ -0,0 +1,31 @@
+import nodemailer from "nodemailer";
+
+
+export async function sendOtpEmail(email: string, otp: string) {
+    const transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+            user: process.env.GMAIL_USER,
+            pass: process.env.GMAIL_PASS,
+        },
+        tls: {
+            rejectUnauthorized: false,
+        }
+    })
+    
+    const htmlTemplate = `
+    
+    `;
+
+    const mailOptions = {
+        from: process.env.EMAIL_USER,
+        to: email,
+        subject: "Your Verification Code - Community Barter",
+        html: htmlTemplate,
+        text: `
+        
+        `,
+    }
+
+    await transporter.sendMail(mailOptions);
+}
