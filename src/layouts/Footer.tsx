8d92c3c28592c75d1402ff61650544a405764df8
Harold Aldovino <haroldaldovino@gmail.com>
2025-08-16 12:02:12 +0800

    .

--- src/layouts/Footer.tsx
+++ src/layouts/Footer.tsx
@@ -1,11 +1,11 @@
 function Footer() {
 	return (
-		<footer className="py-6 mt-12 bg-light text-dark border-t border-gray-300 inset-shadow-gray-50">
+		<footer className="py-6 mt-12 bg-dark-background text-dark-foreground border-t border-gray-300 inset-shadow-gray-50">
 			<div className="max-w-6xl mx-auto px-6 flex flex-col md:flex-row justify-center items-center gap-4">
 				<p className="text-sm">
 					© {new Date().getFullYear()}
-					<span className="text-primary"> Community Barter</span>. All
-					rights reserved.
+					<span className="text-primary-dark"> Palitan Tayo </span>.
+					All rights reserved.
 				</p>
 			</div>
 		</footer>
